1c2f0ff5f40b70bd8c4b2bf34a035d8d4d792588
Artur Morais Brasileiro <arturbrasileiro00@gmail.com>
2025-01-14 11:54:20 -0300

    Exceções com TryCatch
    Trabalhando com exceções com TryCatch no C#

---
+++ Exceções com TryCatch/Program.cs
@@ -0,0 +1,29 @@
+﻿try
+{
+    string[] linhas = File.ReadAllLines("Arquivos/arquivosLeitura.txt");
+
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+
+}
+catch(FileNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado. {ex.Message}");
+}
+catch(DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Caminho da pasta não encontrado. {ex.Message}");
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Ocorreu um erro genérico. {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("O código passou do try catch!");
+}
+
+
+
