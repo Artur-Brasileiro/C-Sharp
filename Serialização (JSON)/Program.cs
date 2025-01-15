cf15079f3029e1f8eb92af0ec316c43f1f929181
Artur Morais Brasileiro <arturbrasileiro00@gmail.com>
2025-01-15 11:31:28 -0300

    Serialização no C#
    Entendendo o funcionamento de um arquivo JSON, serializando e deserializando dados com o pacote Newtonsoft.Json do Nuget.

---
+++ Serialização (JSON)/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Serialização__JSON_.Models;
+
+//SERIALIZANDO UM ARQUIVO
+// List<Venda> listaVendas = new List<Venda>();
+
+// DateTime dataAtual = DateTime.Now;
+
+// Venda v1 = new Venda(1, "Material de Escritório", 25.00M, dataAtual);
+// Venda v2 = new Venda(2, "Licença de Software", 110.00M, dataAtual);
+
+// listaVendas.Add(v1);
+// listaVendas.Add(v2);
+
+// string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+// File.WriteAllText("Arquivos/vendas.json", serializado);
+
+// Console.WriteLine(serializado);
+
+//DESERIALIZANDO UM ARQUIVO
+string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
+
+List<Venda> listaVendas = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+
+foreach (Venda venda in listaVendas)
+{
+    Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Preço: {venda.Preco:C}, Data: {venda.DataVenda.ToString("dd/MM/yyyy HH:mm")}");
+}
