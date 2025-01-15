42b19bed95eb9a6cad66fc373ca729fcd4fa5653
Artur Morais Brasileiro <arturbrasileiro00@gmail.com>
2025-01-15 08:03:50 -0300

    IF Ternário no C#

---
+++ IF Ternário/Program.cs
@@ -0,0 +1,18 @@
+﻿//IF NORMAL
+// int numero = 16;
+
+// if(numero % 2 == 0)
+// {
+//     Console.WriteLine($"O numero {numero} é par");
+// }
+// else
+// {
+//     Console.WriteLine($"O numero {numero} é ímpar");
+// }
+
+//IF TERNÁRIO
+int numero = 16;
+bool ehPar;
+
+ehPar = numero % 2 == 0;
+Console.WriteLine($"O número {numero} é " + (ehPar ? "par" : "ímpar"));
