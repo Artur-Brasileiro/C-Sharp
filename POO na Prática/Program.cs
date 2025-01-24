25fa1475f935d1b2775b40d3c98d8632a9c78f80
Artur Morais Brasileiro <arturbrasileiro00@gmail.com>
2025-01-24 13:39:41 -0300

    Herança e Polimorfismo em C#

--- POO na Prática/Program.cs
+++ POO na Prática/Program.cs
@@ -7,7 +7,20 @@
 // p1.Apresentar();
 
 // Exemplo Encapsulamento
-ContaCorrente c1 = new ContaCorrente(123, 1000);
-c1.ExibirSaldo();
-c1.Sacar(500);
-c1.ExibirSaldo();
+// ContaCorrente c1 = new ContaCorrente(123, 1000);
+// c1.ExibirSaldo();
+// c1.Sacar(500);
+// c1.ExibirSaldo();
+
+// Exemplo Herança e Polimorfismo
+Aluno a1 = new Aluno();
+a1.Nome = "Artur";
+a1.Idade = 20;
+a1.Nota = 10;
+a1.Apresentar();
+
+Professor p1 = new Professor();
+p1.Nome = "Leonardo";
+p1.Idade = 24;
+p1.Salario = 1000;
+p1.Apresentar();
